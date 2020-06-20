9f8d6d792d647e0b0323e6fcd6850f07c2ed091a
Tihan Pelser <pelsertihan@gmail.com>
2020-06-20 14:38:20 +0200

    Added hyperparameters; Added sample results

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# Dimensionality reduction with an autoencoder
-A small experiment in dimensionality reduction using an Autoencoder, with the results compared to that of Principal Component Analysis.
-The tests dataset used is the Iris dataset.
+# Dimensionality Reduction using an Autoencoder and Principal Component Analysis
+A small experiment in dimensionality reduction using an Autoencoder, with the results compared to that of Principal 
+Component Analysis. The test dataset used is the Iris dataset (from Seaborn's built-in datasets).
+
+The graph below shows a sample of the results obtained, illustrating he linear separability of the features in 
+lower dimensions.
+
+![Sample Results](results/sample_results_2.png "Sample Results")
+
+The Autoencoder is created using Keras' Sequential API, while the Principal Component decomposition is done using
+SciKit-Learn's built-in PCA functionality. 
+
+### Running the script:
+Install the necessary requirements by running:
+
+`pip install -r requirements.txt`
+
+Execute the `autoencoder` module by running:
+
+`python -m autoencoder`
+
+The script will produce a graph similar to those given in the `results` folder, and will save generated data 
+(principal components and encoded features) to a __.csv__ file.
+
+The `__main__.py` file inside the `autoencoder` module contains parameters which can be customised to change the number
+of nodes in the hidden layers, the number of encoded features/principal components to generate (i.e. the reduced 
+dimension size of the patterns), etc.
--- autoencoder/__main__.py
+++ autoencoder/__main__.py
@@ -2,65 +2,112 @@ import seaborn as sns
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from typing import Tuple
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import PCA
+import click
+
+NUMBER_OF_REDUCED_FEATURES = 2
+ENCODER_HIDDEN_LAYER_NODES = 20
+DECODER_HIDDEN_LAYER_NODES = 20
+
+OPTIMIZER = "adagrad"
+LOSS = "mse"
+N_EPOCHS = 50
+BATCH_SIZE = 16
 
 
 def pca_decomposition(data):
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=NUMBER_OF_REDUCED_FEATURES)
     return pca.fit_transform(data)
 
 
-def scale_features(iris_dataset):
-    features = iris_dataset.columns.to_list()
-    features.pop(features.index("species"))
+def scale_features(features):
     scaler = MinMaxScaler()
-    scaler.fit(iris_dataset[features])
-    return pd.DataFrame(scaler.transform(iris_dataset[features]), columns=features).to_numpy()
+    return scaler.fit_transform(features)
 
 
-if __name__ == '__main__':
-    iris = sns.load_dataset("iris")
-    x_train = scale_features(iris_dataset=iris)
-
-    input_layer = tf.keras.layers.Dense(20, input_shape=(4,))
-    encoder_output = tf.keras.layers.Dense(3)
-    decoder_input = tf.keras.layers.Dense(20)
+def create_models() -> Tuple[tf.keras.Model, tf.keras.Model, tf.keras.Model]:
+    input_layer = tf.keras.layers.Input(shape=(4,))
+    encoder_input = tf.keras.layers.Dense(ENCODER_HIDDEN_LAYER_NODES)
+    encoder_output = tf.keras.layers.Dense(NUMBER_OF_REDUCED_FEATURES)
+    decoder_input = tf.keras.layers.Dense(DECODER_HIDDEN_LAYER_NODES,)
     decoder_output = tf.keras.layers.Dense(4)
 
     autoencoder = tf.keras.Sequential()
     autoencoder.add(layer=input_layer)
+    autoencoder.add(layer=encoder_input)
     autoencoder.add(layer=encoder_output)
     autoencoder.add(layer=decoder_input)
     autoencoder.add(layer=decoder_output)
-    autoencoder.compile(optimizer="adagrad", loss="mse")
+    autoencoder.compile(optimizer=OPTIMIZER, loss=LOSS)
 
     encoder = tf.keras.Sequential()
     encoder.add(input_layer)
-    encoder.add(encoder_output)
-    encoder.compile(optimizer="adagrad", loss="mse")
+    encoder.add(layer=encoder_input)
+    encoder.add(layer=encoder_output)
+    encoder.compile(optimizer=OPTIMIZER, loss=LOSS)
 
     decoder = tf.keras.Sequential()
-    decoder.add(decoder_input)
-    decoder.add(decoder_output)
-    decoder.compile(optimizer="adagrad", loss="mse")
+    decoder.add(layer=decoder_input)
+    decoder.add(layer=decoder_output)
+    decoder.compile(optimizer=OPTIMIZER, loss=LOSS)
 
-    autoencoder.fit(x=x_train, y=x_train, epochs=500, verbose=True, batch_size=8)
+    return autoencoder, encoder, decoder
+
+
+def main():
+    iris = sns.load_dataset("iris")
+    features = iris.columns.to_list()
+    features.remove("species")
+
+    x_train = scale_features(iris[features])
+
+    autoencoder, encoder, decoder = create_models()
+
+    autoencoder.fit(x=x_train, y=x_train, epochs=N_EPOCHS, batch_size=BATCH_SIZE)
 
     encoded_data = encoder.predict(x_train)
     decoded_data = decoder.predict(encoded_data)
 
     loss = mean_squared_error(y_true=x_train, y_pred=decoded_data)
-    print(loss)
+    print(f"Reconstruction MSE = {loss}")
 
-    encoded_df = pd.DataFrame(data=encoded_data, columns=["f1", "f2", "f3"])
+    encoded_feature_labels = [f"f{i+1}" for i in range(NUMBER_OF_REDUCED_FEATURES)]
+
+    encoded_df = pd.DataFrame(data=encoded_data, columns=encoded_feature_labels)
     encoded_df["species"] = iris["species"]
+    encoded_df["method"] = ["encoder" for _ in range(len(encoded_df))]
 
-    pca_decomp = pd.DataFrame(data=pca_decomposition(iris[features]), columns=["pc1", "pc2"])
-    pca_decomp["species"] = iris["species"]
+    pca_df = pd.DataFrame(data=pca_decomposition(iris[features]), columns=encoded_feature_labels)
+    pca_df["species"] = iris["species"]
+    pca_df["method"] = ["pca" for _ in range(len(pca_df))]
 
-    ax = sns.scatterplot(x="f1", y="f2", hue="species", data=encoded_df)
-    ax = sns.scatterplot(x="f1", y="f3", style="species", data=encoded_df, ax=ax)
+    reduced_data = pd.concat([encoded_df, pca_df], ignore_index=True)
+    reduced_data.to_csv("reduced_data.csv")
+
+    if NUMBER_OF_REDUCED_FEATURES > 1:
+        fig = plt.figure(1)
+        ax = sns.scatterplot(x=encoded_feature_labels[0],
+                             y=encoded_feature_labels[1],
+                             hue="species",
+                             style="method",
+                             data=reduced_data)
+
+        ax.set_title("Comparison of Principal Components\n and Autoencoder Encoded Data")
+
+    fig = plt.figure(2)
+    ax = sns.scatterplot(x=encoded_feature_labels[0],
+                         y=encoded_feature_labels[1],
+                         hue="species",
+                         data=encoded_df)
+
+    ax.set_title("Comparison of Principal Components\n and Autoencoder Encoded Data")
+
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
